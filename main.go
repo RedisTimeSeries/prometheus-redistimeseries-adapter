07ac0d723b7383c52ba84388445103e4ebf6054c
Roey Prat <roey.prat@redislabs.com>
2018-11-13 12:06:14 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,250 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/snappy"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/prometheus/prompb"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+type config struct {
+	redisAddress  string
+	redisPort     int
+	redisAuth     string
+	remoteTimeout time.Duration
+	listenAddr    string
+	telemetryPath string
+	logLevel      string
+}
+
+var (
+	receivedSamples = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "received_samples_total",
+			Help: "Total number of received samples.",
+		},
+	)
+	sentSamples = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "sent_samples_total",
+			Help: "Total number of processed samples sent to remote storage.",
+		},
+		[]string{"remote"},
+	)
+	failedSamples = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "failed_samples_total",
+			Help: "Total number of processed samples which failed on send to remote storage.",
+		},
+		[]string{"remote"},
+	)
+	sentBatchDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "sent_batch_duration_seconds",
+			Help:    "Duration of sample batch send calls to the remote storage.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"remote"},
+	)
+)
+
+func parseFlags() *config {
+	cfg := &config{
+		redisAuth: os.Getenv("REDIS_AUTH"),
+	}
+
+	flag.StringVar(&cfg.redisAddress, "redis-address", "localhost:6379",
+		"The host:port of the Redis server to send samples to. localhost:6379, if empty.",
+	)
+	flag.StringVar(&cfg.redisAuth, "redis-auth", "",
+		"The Redis authentication string. empty string, if empty.",
+	)
+	flag.DurationVar(&cfg.remoteTimeout, "send-timeout", 30*time.Second,
+		"The timeout to use when sending samples to the remote storage.",
+	)
+	flag.StringVar(&cfg.listenAddr, "web.listen-address", ":9201", "Address to listen on for web endpoints.")
+	flag.StringVar(&cfg.telemetryPath, "web.telemetry-path", "/metrics", "Address to listen on for web endpoints.")
+	flag.StringVar(&cfg.logLevel, "log.level", "debug", "Only log messages with the given severity or above. One of: [debug, info, warn, error]")
+
+	flag.Parse()
+
+	return cfg
+}
+
+type writer interface {
+	Write(samples model.Samples) error
+	Name() string
+}
+
+type reader interface {
+	Read(req *prompb.ReadRequest) (*prompb.ReadResponse, error)
+	Name() string
+}
+
+func buildClients(logger log.Logger, cfg *config) ([]writer, []reader) {
+	var writers []writer
+	var readers []reader
+	// TODO: build redis client here
+	level.Info(logger).Log("msg", "Starting up...")
+	return writers, readers
+}
+
+func protoToSamples(req *prompb.WriteRequest) model.Samples {
+	var samples model.Samples
+	for _, ts := range req.Timeseries {
+		metric := make(model.Metric, len(ts.Labels))
+		for _, l := range ts.Labels {
+			metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+		}
+
+		for _, s := range ts.Samples {
+			samples = append(samples, &model.Sample{
+				Metric:    metric,
+				Value:     model.SampleValue(s.Value),
+				Timestamp: model.Time(s.Timestamp),
+			})
+		}
+	}
+	return samples
+}
+
+func serve(logger log.Logger, addr string, writers []writer, readers []reader) error {
+	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
+		compressed, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			level.Error(logger).Log("msg", "Read error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		reqBuf, err := snappy.Decode(nil, compressed)
+		if err != nil {
+			level.Error(logger).Log("msg", "Decode error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req prompb.WriteRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			level.Error(logger).Log("msg", "Unmarshal error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		samples := protoToSamples(&req)
+		receivedSamples.Add(float64(len(samples)))
+
+		var wg sync.WaitGroup
+		for _, w := range writers {
+			wg.Add(1)
+			go func(rw writer) {
+				sendSamples(logger, rw, samples)
+				wg.Done()
+			}(w)
+		}
+		wg.Wait()
+	})
+
+	http.HandleFunc("/read", func(w http.ResponseWriter, r *http.Request) {
+		compressed, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			level.Error(logger).Log("msg", "Read error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		reqBuf, err := snappy.Decode(nil, compressed)
+		if err != nil {
+			level.Error(logger).Log("msg", "Decode error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		var req prompb.ReadRequest
+		if err := proto.Unmarshal(reqBuf, &req); err != nil {
+			level.Error(logger).Log("msg", "Unmarshal error", "err", err.Error())
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// TODO: Support reading from more than one reader and merging the results.
+		if len(readers) != 1 {
+			http.Error(w, fmt.Sprintf("expected exactly one reader, found %d readers", len(readers)), http.StatusInternalServerError)
+			return
+		}
+		reader := readers[0]
+
+		var resp *prompb.ReadResponse
+		resp, err = reader.Read(&req)
+		if err != nil {
+			level.Warn(logger).Log("msg", "Error executing query", "query", req, "storage", reader.Name(), "err", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		data, err := proto.Marshal(resp)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/x-protobuf")
+		w.Header().Set("Content-Encoding", "snappy")
+
+		compressed = snappy.Encode(nil, data)
+		if _, err := w.Write(compressed); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	return http.ListenAndServe(addr, nil)
+}
+
+func init() {
+	prometheus.MustRegister(receivedSamples)
+	prometheus.MustRegister(sentSamples)
+	prometheus.MustRegister(failedSamples)
+	prometheus.MustRegister(sentBatchDuration)
+}
+
+func main() {
+	cfg := parseFlags()
+	http.Handle(cfg.telemetryPath, promhttp.Handler())
+
+	logLevel := promlog.AllowedLevel{}
+	if err := logLevel.Set(cfg.logLevel); err != nil {
+		panic(fmt.Sprintf("Error setting log level: %v", err))
+	}
+	logger := promlog.New(logLevel)
+
+	writers, readers := buildClients(logger, cfg)
+	if err := serve(logger, cfg.listenAddr, writers, readers); err != nil {
+		level.Error(logger).Log("msg", "Failed to listen", "addr", cfg.listenAddr, "err", err)
+		os.Exit(1)
+	}
+}
+
+func sendSamples(logger log.Logger, w writer, samples model.Samples) {
+	begin := time.Now()
+	err := w.Write(samples)
+	duration := time.Since(begin).Seconds()
+	if err != nil {
+		level.Warn(logger).Log("msg", "Error sending samples to remote storage", "err", err, "storage", w.Name(), "num_samples", len(samples))
+		failedSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+	}
+	sentSamples.WithLabelValues(w.Name()).Add(float64(len(samples)))
+	sentBatchDuration.WithLabelValues(w.Name()).Observe(duration)
+}
