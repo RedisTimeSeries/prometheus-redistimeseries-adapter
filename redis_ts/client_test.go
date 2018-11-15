975d34fca46f0b1b91c7408decb6bd6bf5c8835c
Slava Koyfman <slava.koyfman@redislabs.com>
2018-11-15 14:04:52 +0200

    Extend "unit" test

--- redis_ts/client_test.go
+++ redis_ts/client_test.go
@@ -1,39 +1,54 @@
 package redis_ts
 
 import (
+	"github.com/RedisLabs/redis-timeseries-go"
 	"github.com/garyburd/redigo/redis"
 	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
 	"testing"
-	"time"
 )
 
 const redisAddress = "127.0.0.1:6379"
 
-var redisPool *redis.Pool = &redis.Pool{
-	MaxIdle:     3,
-	IdleTimeout: 240 * time.Second,
-	Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", redisAddress) },
-}
-var remoteClient *Client = NewClient(nil, redisAddress, "")
+var redisTS = redis_timeseries_go.NewClient(redisAddress, "noname")
 
 func TestWriteSingleSample(t *testing.T) {
+	conn, err := redis.Dial("tcp", redisAddress)
+	require.Nil(t, err, "Could not connect to Redis")
+	defer conn.Close()
+	defer conn.Do("FLUSHALL")
+
+	now := model.Now()
+	answerToLifeTheUniverse := 42.0
+
+	var seriesName model.LabelValue = "test_series"
+
 	samples := model.Samples{
 		&model.Sample{
 			Metric: model.Metric{
-				model.LabelName("label_1"): model.LabelValue("value_1"),
-				model.LabelName("label_2"): model.LabelValue("value_2"),
+				model.MetricNameLabel: seriesName,
+				"label_1":             "value_1",
+				"label_2":             "value_2",
 			},
-			Value:     42.0,
-			Timestamp: model.Now(),
+			Value:     model.SampleValue(answerToLifeTheUniverse),
+			Timestamp: now,
 		},
 	}
 
-	err := remoteClient.Write(samples)
-	require.Nil(t, err, "Write of samples should succeed")
+	var remoteClient = NewClient(nil, redisAddress, "")
 
-	conn := redisPool.Get()
-	defer conn.Close()
+	err = remoteClient.Write(samples)
+	require.Nil(t, err, "Write of samples failed")
 
-	t.Log(conn.Do("KEYS", "*"))
+	dataPoints, err := redisTS.Range(string(seriesName), 0, now.Unix())
+	require.Nil(t, err, "Failed getting samples from Redis")
+	require.Len(t, dataPoints, 1, "Incorrect number of samples in Redis")
+	dp := dataPoints[0]
+	require.Equal(t,
+		redis_timeseries_go.DataPoint{
+			Timestamp: now.Unix(),
+			Value: answerToLifeTheUniverse,
+		},
+		dp,"Unexpected sample in Redis",
+	)
 }
